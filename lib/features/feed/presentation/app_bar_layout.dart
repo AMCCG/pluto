86cb7bc1ffceaf4ac00f27f1e00eab25e58264d3
apisit.a <amornchanchaigul@gmail.com>
2022-09-14 22:01:03 +0700

    add PreferredSize to appbar

--- lib/features/feed/presentation/app_bar_layout.dart
+++ lib/features/feed/presentation/app_bar_layout.dart
@@ -14,6 +14,7 @@ class AppBarLayout {
           color: Colors.blue,
         ),
       ),
+      bottom: _generatePreferredSize(),
       actions: [
         Container(
           alignment: Alignment.center,
@@ -31,6 +32,72 @@ class AppBarLayout {
           ),
         )
       ],
+    );
+  }
+
+  static PreferredSize _generatePreferredSize() {
+    return PreferredSize(
+      preferredSize: const Size.fromHeight(50),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            child: Container(
+              height: 50,
+              child: TextButton(
+                onPressed: () {},
+                child: const Icon(Icons.home, color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 50,
+              child: TextButton(
+                onPressed: () {},
+                child: const Icon(Icons.group, color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 50,
+              child: TextButton(
+                onPressed: () {},
+                child: const Icon(Icons.ondemand_video_rounded,
+                    color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 50,
+              child: TextButton(
+                onPressed: () {},
+                child: const Icon(Icons.feed_outlined, color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 50,
+              child: TextButton(
+                onPressed: () {},
+                child: const Icon(Icons.notifications, color: Colors.black),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(
+              height: 50,
+              child: TextButton(
+                onPressed: () {},
+                child: const Icon(Icons.menu, color: Colors.black),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
